e05e6716cc6dcd2bd00ef78c26da1f5b42db1474
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-12-07 14:57:58 -0600

    first lecture

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // implement your API here
+
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+const PORT = 4000;
+
+//endpoint
+
+server.get('/api/users', (req,res) => {
+  db.find()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(err => {
+
+      res
+        .status(500)
+        .json({message: 'failed to get users'})
+    })
+})
+
+server.get('/api/users/:id', (req,res) => {
+  const {id} = req.params;
+  db.findById(id)
+    .then(user => {
+      if (user){
+        res.json(user);
+      } else {
+        res
+          .status(404)
+          .json({message: 'user does not exist'})
+      }
+    })
+    .catch(err => { 
+      res
+        .status(404)
+        .json({message: "failed to get user"})
+    })
+})
+
+//Listening
+server.listen(PORT, () => {
+  console.log(`Server running on Port ${PORT}`)
+});
